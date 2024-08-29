337cafbb5d4ed4c66c12361c0ca401866ba8ae92
Tess HF <101187655+higher-bottle@users.noreply.github.com>
2024-08-30 04:24:54 +0800

    Update Procfile

---
+++ wsgi.py
@@ -0,0 +1,9 @@
+from Ibook import create_app
+from dotenv import load_dotenv
+import os
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+app = create_app()
